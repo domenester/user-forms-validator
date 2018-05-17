3854f40825a8172fd6ccf7188b3af5d35904bdfb
Diogo Domene <domenester@gmail.com>
2018-05-16 23:28:54 -0300

    Inserting some enums

--- src/signup/enum.js
+++ src/signup/enum.js
@@ -1,6 +1,34 @@
 const userEnum = {
   INPUT_NAME: {
     label: 'Nome',
+    type: 'string',
+    field: 'input',
+    length: {
+      min: 2,
+      max: 20,
+    },
+  },
+  INPUT_LASTNAME: {
+    label: 'Sobrenome',
+    type: 'string',
+    field: 'input',
+    length: {
+      min: 2,
+      max: 20,
+    },
+  },
+  INPUT_BIRTH: {
+    label: 'Data de Nascimento',
+    type: 'date',
+    field: 'input',
+  },
+  INPUT_EMAIL: {
+    label: 'E-mail',
+    type: 'string',
+    field: 'input',
+    data: {
+      subType: 'email',
+    },
   },
 };
 
